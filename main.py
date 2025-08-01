1a194c059533fe15af0fcd152a811ea13c5238a3
adamadecode <damsonduweb@gmail.com>
2025-08-01 21:57:16 +0000

    words counter

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_contents(path):
+	with open(path) as f:
+		return f.read() 
+
+def main():
+	num_words= words_counter(get_book_contents("books/frankenstein.txt"))
+	print(num_words)
+
+def words_counter(book_contents):
+	num_words= len(book_contents.split())
+	return f"{num_words} words found in the document"
+main()
